ff1d6f8963123480fda1afa1df43503ae905d77c
Wendell Gabriel Fernandes Barros <102330689+mrwendellbarros@users.noreply.github.com>
2022-10-28 17:41:29 -0300

    implementação dos mapas p1

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+// Mapas
+/*
+var mapreg =  document.querySelector("#mapgo");
+let select = document.querySelector("#selectUf")
+let valor = select.options.value
+
+console.log (valor)
+
+function openMap() {
+  switch (select) {
+    case select.value[0]:
+      mapreg.style.display = "none";
+      break;
+    default:
+      break;
+  }
+}*/
+/*for ( var i = 0; i < select.options.length; i++) {
+  switch (select.options.value) {
+    case "2":
+      mapreg.style.display = "none"
+      break
+  }
+}*/
